98682aa27c5f4b5460ddcb2a61bf58beb9a759eb
bestversionurself-source <bestversionurself@gmail.com>
2026-08-15 14:22:25 +0530

    Use verified Font Awesome integrity hash

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
 <!doctype html><html lang="en"><head>
 <meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1">
 <title><?= e($title) ?></title><meta name="description" content="Web design, PHP development, e-commerce and digital growth services from Reliance Digital Agency.">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKB4Imkb9hVqc7m5R4w5ZzOqLz9+3S2yA7xjGkGgV7Hj1kGmM7uJ9y2uQ5T5S5n5X5p5Q==" crossorigin="anonymous" referrerpolicy="no-referrer">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer">
 <link rel="stylesheet" href="/assets/css/style.css"><?php if ($page === 'privacy'): ?><link rel="stylesheet" href="/assets/css/privacy.css"><?php endif; ?><script defer src="/assets/js/site.js"></script>
 </head><body>
 <div class="topbar" aria-hidden="true"><div class="wrap"></div></div>
